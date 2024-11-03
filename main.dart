bb48e8069166d7c50fd446764281a437b5643cd1
Suleman25 <sulemanjamil177@gmail.com>
2024-11-03 21:31:06 +0500

    1

--- main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_assignment_2/nested_boxes.dart';
-void main (){
-   runApp(const MaterialApp(
-    home: Scaffold(
-      body: NestedBoxes(),
-    ),
-   ));
-}
